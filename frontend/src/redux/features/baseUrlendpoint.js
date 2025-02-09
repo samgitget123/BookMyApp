c3eafea4e5b5a2bd098312f9e723942031432329
sampath <sampathgoud451@gmail.com>
2025-02-09 20:07:28 +0530

    pushing re-build code BMG_V_1.O

--- frontend/src/redux/features/baseUrlendpoint.js
+++ frontend/src/redux/features/baseUrlendpoint.js
@@ -1,4 +1,4 @@
 // baseUrlEndpoint.js
 //https://bookmyground.onrender.com
 //http://localhost:5000
-export const baseUrl = `https://bookmyground.onrender.com`;
+export const baseUrl = `https://bookingapp-v-1-0.onrender.com`;
