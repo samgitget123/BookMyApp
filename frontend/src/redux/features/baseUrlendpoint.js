da4af56b21b9a6d50f5b27db3781b405e22899ff
sampath <sampathgoud451@gmail.com>
2025-03-08 06:38:39 +0530

    commiting new dev code and build

--- frontend/src/redux/features/baseUrlendpoint.js
+++ frontend/src/redux/features/baseUrlendpoint.js
@@ -1,4 +1,4 @@
 // baseUrlEndpoint.js
 //https://bookingapp-v-1-0.onrender.com`
 //http://localhost:5000
-export const baseUrl = `http://localhost:5000`;
+export const baseUrl = `https://bookingapp-v-1-0.onrender.com`;
